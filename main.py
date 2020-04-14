f559ed8f05e3abef0eb4192778d57b61a7909f0e
Nebelhom <nebelhom@hotmail.com>
2020-04-14 21:38:55 +0200

    updated run method to flip through images
    removed os.path because it was unused
    
    First steps. I achieved scaling within aspect ratio.
    Still needs to centre the image and clean up the code.
    
    Images are scaled according to resolution
    helper functions moved into helper_func.py
    
    Added center_img function to helper_func.py to allow centering of newly scaled image
    
    Cleaned up code in 'while running' as redundancies were present

--- main.py
+++ main.py
@@ -2,72 +2,75 @@
 
 from os import listdir, getcwd
 import random
-import os.path as osp
 
 import pygame
 from pygame.locals import *
+
+import helper_func as hf
 
 pygame.init()
 
 FORMATS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'pcx', 'tga', 'tif', 'lbm',
            'pbm', 'pgm', 'ppm', 'xpm']
 
-def list_img_paths(cpath):
-    """
-    Lists all image filenames in the path directory, p.
-
-    curpath: String: Correctly formatted path according to platform
-
-    Returns:
-    List of strings of image filenames.
-    """
-
-    return [i for i in listdir(cpath) if i.split('.')[-1].lower() in FORMATS]
+# Obtain the current display info for scaling of the image
+# see line below pygame.transform.scale
+infoObject = pygame.display.Info()
+SCREEN_RES = (infoObject.current_w, infoObject.current_h)
 
 
 class SlideShow:
     """
     Write proper documentation!!!
-
     Runs slideshow using pygame
-
     """
 
-    def __init__(self, imgpath=getcwd()):
-        self.imgsrc = list_img_paths(imgpath)
-        self.img = random.choice(self.imgsrc)
+    def __init__(self, time_delay=3000, path2imgs=getcwd()):
+        # Sorting out the images
+        self.imgsrc = hf.list_img_paths(path2imgs)
+        self.imgpath = random.choice(self.imgsrc)
+        # will be populated as pygame.Surface in run method
+        self.img = None
+
+        # Defines how much time passes between image switch
+        self.time_delay = time_delay
+
 
     def run(self):
         """
         DEFINITION
-
         TODOs
-        * Set up the github for version control and project structure
-        * make image scaled to screen
         * Deal with rotation images (orientation landscape vs. portrait)
             * Most likely decide on landscape or portrait and pick a good screen.
         * Switch the image and regular intervals (X Minutes)
-
         * After that look into motion sensor with Raspberry Pi Zero
         * The sleep function for Raspberry and wake up
-
         """
 
-        # Make image now scaled to surface and resizable acording to screen size
-        # https://stackoverflow.com/questions/20002242/how-to-scale-images-to-screen-size-in-pygame
+        black = (0, 0, 0)
 
-        img = pygame.image.load(self.img)
-
-        white = (255, 64, 64)
+        # Chooses the size of your display
+        # Makes it portable to varying screens
         size = (0, 0)
         screen = pygame.display.set_mode(size, FULLSCREEN)
-        screen.fill((white))
+
         running = True
 
         while running:
-            screen.fill((white))
-            screen.blit(img, (0, 0))
+            self.imgpath = random.choice(self.imgsrc)
+            self.img = pygame.image.load(self.imgpath)
+
+            # Scale the image according to screen resolution
+            new_dim = hf.aspect_scale(self.img.get_size(), SCREEN_RES)
+            self.img = pygame.transform.scale(self.img, new_dim)
+            # get the coordinates to center image
+            centre_coord = hf.center_img(self.img.get_size(), SCREEN_RES)
+
+            screen.fill(black)
+            screen.blit(self.img, centre_coord)
+            pygame.time.delay(self.time_delay)
             pygame.display.flip()
+
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 # only do something if the event is of type QUIT
@@ -83,4 +86,3 @@ class SlideShow:
 if __name__ == "__main__":
     sl = SlideShow()
     sl.run()
-    
