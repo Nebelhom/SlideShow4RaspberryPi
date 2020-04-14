56d3de360b02591df120a783741ab3664d04b3f4
Nebelhom <nebelhom@hotmail.com>
2020-04-14 21:38:55 +0200

    Update main.py
    Addresses issue #4
    
    updated run method to flip through images

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ FORMATS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'pcx', 'tga', 'tif', 'lbm',
 def list_img_paths(cpath):
     """
     Lists all image filenames in the path directory, p.
-
     curpath: String: Correctly formatted path according to platform
-
     Returns:
     List of strings of image filenames.
     """
@@ -28,29 +26,26 @@ def list_img_paths(cpath):
 class SlideShow:
     """
     Write proper documentation!!!
-
     Runs slideshow using pygame
-
     """
 
     def __init__(self, imgpath=getcwd()):
         self.imgsrc = list_img_paths(imgpath)
         self.img = random.choice(self.imgsrc)
+        self.imgpath = imgpath
+
 
     def run(self):
         """
         DEFINITION
-
         TODOs
         * Set up the github for version control and project structure
         * make image scaled to screen
         * Deal with rotation images (orientation landscape vs. portrait)
             * Most likely decide on landscape or portrait and pick a good screen.
         * Switch the image and regular intervals (X Minutes)
-
         * After that look into motion sensor with Raspberry Pi Zero
         * The sleep function for Raspberry and wake up
-
         """
 
         # Make image now scaled to surface and resizable acording to screen size
@@ -58,16 +53,24 @@ class SlideShow:
 
         img = pygame.image.load(self.img)
 
-        white = (255, 64, 64)
+        white = (0, 0, 0)
         size = (0, 0)
         screen = pygame.display.set_mode(size, FULLSCREEN)
         screen.fill((white))
         running = True
 
         while running:
+            new_img = random.choice(self.imgsrc)
+
             screen.fill((white))
+
             screen.blit(img, (0, 0))
+            self.img = pygame.image.load(new_img)
+            screen.fill(white)
+            screen.blit(self.img, (0, 0))
+            pygame.time.delay(3000)
             pygame.display.flip()
+
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
                 # only do something if the event is of type QUIT
@@ -83,4 +86,3 @@ class SlideShow:
 if __name__ == "__main__":
     sl = SlideShow()
     sl.run()
-    
