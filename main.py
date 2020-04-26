5055970985d05b2de49feac56386ac47c6076bfe
Nebelhom <nebelhom@hotmail.com>
2020-04-26 16:35:40 +0200

    Update main.py
    Addresses issue #10
    
    Created Change_image(self)
    Included threading.Timer
    Calls self.time2switch as flag to change
    
    Investigate if better thread garbage collection is needed. Each new thread receives a number starting at 1 and counting. If left to run, what if we are at Thread-<HUGE NUMBER>. Problem?

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from os import listdir, getcwd
 import random
+import threading as th
 
 import pygame
 from pygame.locals import *
@@ -12,6 +13,7 @@ pygame.init()
 
 FORMATS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'pcx', 'tga', 'tif', 'lbm',
            'pbm', 'pgm', 'ppm', 'xpm']
+BLACK = (0, 0, 0)
 
 # Obtain the current display info for scaling of the image
 # see line below pygame.transform.scale
@@ -25,7 +27,7 @@ class SlideShow:
     Runs slideshow using pygame
     """
 
-    def __init__(self, time_delay=3000, path2imgs=getcwd()):
+    def __init__(self, time_delay=3, path2imgs=getcwd()):
         # Sorting out the images
         self.imgsrc = hf.list_img_paths(path2imgs)
         self.imgpath = random.choice(self.imgsrc)
@@ -34,20 +36,31 @@ class SlideShow:
 
         # Defines how much time passes between image switch
         self.time_delay = time_delay
+        self.time2switch = True
 
+    def change_image(self):
+        """
+        Updates the path to picture.
+        """
+
+        self.imgpath = random.choice(self.imgsrc)
+
+    def signal_switch_image(self):
+        """
+        Called by Timer to switch when time is up
+        """
+        self.time2switch = True
 
     def run(self):
         """
         DEFINITION
         TODOs
         * Deal with rotation images (orientation landscape vs. portrait)
-            * Most likely decide on landscape or portrait and pick a good screen.
+         * Most likely decide on landscape or portrait and pick a good screen.
         * Switch the image and regular intervals (X Minutes)
         * After that look into motion sensor with Raspberry Pi Zero
         * The sleep function for Raspberry and wake up
         """
-
-        black = (0, 0, 0)
 
         # Chooses the size of your display
         # Makes it portable to varying screens
@@ -57,7 +70,23 @@ class SlideShow:
         running = True
 
         while running:
-            self.imgpath = random.choice(self.imgsrc)
+            if self.time2switch:
+                ## TODO
+                ## Look at repeating timers
+                ## Currently creating a new thread each time...
+                ## Not sure if the numbering will become an issue...
+
+
+                # Changes the image
+                self.change_image()
+
+                # Creates new Timer thread
+                # (Reminder: Cannot restart the same thread)
+                self.time2switch = False
+                timer = th.Timer(self.time_delay, self.signal_switch_image)
+                timer.start()
+                print(th.enumerate())
+
             self.img = pygame.image.load(self.imgpath)
 
             # Scale the image according to screen resolution
@@ -66,10 +95,10 @@ class SlideShow:
             # get the coordinates to center image
             centre_coord = hf.center_img(self.img.get_size(), SCREEN_RES)
 
-            screen.fill(black)
+            screen.fill(BLACK)
             screen.blit(self.img, centre_coord)
-            pygame.time.delay(self.time_delay)
             pygame.display.flip()
+
 
             # event handling, gets all event from the event queue
             for event in pygame.event.get():
